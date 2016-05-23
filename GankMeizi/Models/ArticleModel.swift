d4db78e04ebb7bf665769739d0d12b4fcdb16820
lulee007 <lulee007@live.com>
2016-05-24 00:34:20 +0800

    add article model api :refresh loadmore by page

---
+++ GankMeizi/Models/ArticleModel.swift
@@ -0,0 +1,113 @@
+//
+//  ArticleModel.swift
+//  GankMeizi
+//
+//  Created by 卢小辉 on 16/5/23.
+//  Copyright © 2016年 lulee007. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import CocoaLumberjack
+import ObjectMapper
+
+class ArticleModel: BaseModel {
+    
+    var articleEntities: [ArticleEntity]
+    
+    override init() {
+        articleEntities = []
+        super.init()
+    }
+    
+    
+    /* 刷新 */
+    func refresh() -> Observable<[ArticleEntity]> {
+        page = 1
+        return getArticleInfoByPage(page)
+    }
+    
+    /* 分页获取更多 */
+    func loadMore() -> Observable<[ArticleEntity]> {
+        page += 1
+        return getArticleInfoByPage(page)
+    }
+    
+    //MARK: private method
+    
+//    private func getArticleInfoByPage(page:Int,count:Int) -> Observable<[HtmlArticleEntity]>{
+//        return provider
+//            .request(GankIOService.HtmlByPage(page: page, count: count))
+//            .map({ (response) -> [HtmlArticleEntity] in
+//                let result = Mapper<BaseEntity<HtmlArticleEntity>>().map(String(data: response.data,encoding:  NSUTF8StringEncoding))
+//                let data = (result?.results)!
+//                if page == 1{
+//                    // new or refresh data
+//                    self.articleEntities = data
+//                }else{
+//                    self.articleEntities += data
+//                }
+//                return (result?.results)!
+//            })
+//            
+//            .doOnNext({ (entities) in
+//                if entities.isEmpty {
+//                    self.page -= 1
+//                }else{
+//                    self.page += 1
+//                }
+//            })
+//    }
+    
+    private func getArticleInfoByPage(page:Int) -> Observable<[ArticleEntity]> {
+        return Observable
+            .zip(
+                provider
+                    .request(GankIOService.ByPageAndKind(kind: "福利", page: page, count: self.offset))
+                    .map({ (response) -> [ArticleEntity] in
+                        let result = Mapper<BaseEntity<ArticleEntity>>().map(String(data: response.data,encoding:  NSUTF8StringEncoding))
+                        return (result?.results)!
+                    }),
+                provider.request(GankIOService.ByPageAndKind(kind: "休息视频", page: page, count: self.offset))
+                    .map({ (response) -> [ArticleEntity] in
+                        let result = Mapper<BaseEntity<ArticleEntity>>().map(String(data: response.data,encoding:  NSUTF8StringEncoding))
+                        return (result?.results)!
+                    }),
+                resultSelector: { (girls, videos) -> [ArticleEntity] in
+                    for item in girls{
+                        item.desc = item.desc! + " " + self.getFirstVideoDescByPublishTime(videos,publishedAt: item.publishedAt!)
+                    }
+                    return girls
+                }
+            )
+            .doOnNext({ (entities) in
+                if entities.isEmpty{
+                    self.page -= 1
+                }else{
+                    if self.page == 1{
+                        // new or refresh data
+                        self.articleEntities = entities
+                    }else{
+                        self.articleEntities += entities
+                    }
+                    self.page += 1
+                }
+            })
+    }
+    
+    func getFirstVideoDescByPublishTime(videos: [ArticleEntity],publishedAt:NSDate) -> String {
+        var videoDesc = ""
+        for item in videos {
+            if item.publishedAt == nil {
+                item.publishedAt = item.createdAt
+            }
+            let videoPublishedAt = item.publishedAt
+            if DateUtil.areDatesSameDay(videoPublishedAt!, dateTwo: publishedAt){
+                videoDesc = item.desc!
+                break
+            }
+        }
+        return videoDesc
+    }
+    
+   }
